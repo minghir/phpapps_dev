4306e34474a732c6c80b93918bd1014ce50a5334
minghir <minghir@gmail.com>
2016-09-30 14:44:40 +0300

    https://github.com/minghir/phpapps_dev.git

--- eshop_template/sql/eshop.sql
+++ eshop_template/sql/eshop.sql
@@ -51,42 +51,6 @@ LOCK TABLES `categories` WRITE;
 INSERT INTO `categories` VALUES (1,1,'root','root','',1,1,'2015-08-19 09:39:54','2015-08-19 09:39:54'),(8,1,'a','s','d',1,1,'2015-08-19 09:39:54','2015-08-19 09:39:54'),(14,1,'sa','sa','sa',1,1,'2015-08-19 09:39:54','2015-08-19 09:39:54');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `products` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CATEG_ID` bigint(20) NOT NULL DEFAULT '0',
-  `PRODUCT_NAME` varchar(255) NOT NULL DEFAULT '',
-  `DESCRIPTION` tinytext NOT NULL,
-  `MODIFY_UID` bigint(20) NOT NULL DEFAULT '1',
-  `CREATE_UID` bigint(20) NOT NULL DEFAULT '1',
-  `MODIFY_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`),
-  KEY `CATEG_ID` (`CATEG_ID`),
-  KEY `products_ibfk_2` (`MODIFY_UID`),
-  KEY `products_ibfk_3` (`CREATE_UID`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`CATEG_ID`) REFERENCES `categories` (`ID`),
-  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`MODIFY_UID`) REFERENCES `phpapps`.`users` (`ID`),
-  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`CREATE_UID`) REFERENCES `phpapps`.`users` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,8,'cici','aaa',1,1,'2015-08-19 09:40:59','2015-08-19 09:40:59'),(2,1,'mimisss','fifi',1,1,'2016-08-16 13:28:13','2016-08-16 08:35:07'),(4,1,'aaass','aaaasasasss',1,1,'2016-08-16 13:35:09','2016-08-16 13:33:39'),(9,8,'sadasdddsds2222','ttttttttt',1,1,'2016-08-16 13:40:33','2016-08-16 13:40:22');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -97,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-29 17:24:23
+-- Dump completed on 2016-09-30 14:42:37

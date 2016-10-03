6c0d80fc67eb93ce6c149d81c22f365516d35442
minghir <minghir@gmail.com>
2016-10-03 17:06:22 +0300

    https://github.com/minghir/phpapps_dev.git

--- phpapps/phpapps_database_table_indexes_DDL_form_imp.php
+++ phpapps/phpapps_database_table_indexes_DDL_form_imp.php
@@ -8,6 +8,7 @@ include ("gen_php/phpapps_database_table_indexes_DDL_form.php");
                 public $SCHEMA_NAME;
                 public $TABLE_NAME;
                 public $MODULE_ID;
+                public $INDEX_COLUMNS_LABELS = array();
                 private $table_definition;
             
 		function __construct(){
@@ -16,9 +17,9 @@ include ("gen_php/phpapps_database_table_indexes_DDL_form.php");
                         
                         $this->get_table_id = $_GET["table_id"];
                         $this->TABLE_ID = $this->get_table_id;
-                        $this->MODULE_ID = _tbl("phpapps.tables","MODULE_ID",$this->get_table_id);
+                        $this->MODULE_ID = _tbl("phpapps.view_tables","MODULE_ID",$this->get_table_id);
                         $this->TABLE_NAME =_tbl("phpapps.tables","TABLE_NAME",$this->get_table_id);
-                        $this->SCHEMA_NAME =_tbl("phpapps.tables","TABLE_SCHEMA",$this->get_table_id);
+                        $this->SCHEMA_NAME =_tbl("phpapps.view_tables","TABLE_SCHEMA",$this->get_table_id);
                         
 			$this->init();
 			$this->display();
@@ -31,9 +32,23 @@ include ("gen_php/phpapps_database_table_indexes_DDL_form.php");
 		}
 	
 		function beforeAddRec(){
+                    
+                 echo "AICI<h1>" .$this->SCHEMA_NAME."</h1>";
+                    $sql = new DB_query("SELECT COLUMN_NAME "
+                                        . "FROM phpapps.table_details "
+                                        . "WHERE ID IN (".implode(",",$this->INDEX_COLUMNS).")"
+                                        );
+                    print_r($sql);
+                    if( $this->globals->con->query($sql, $this->table_name) != -1 ){
+		
+			while($res=$this->globals->con->fetch_array($this->table_name)){
+				$this->INDEX_COLUMNS_LABELS[] = $res["COLUMN_NAME"];
+			}
+                    }
+                    print_r($this->INDEX_COLUMNS_LABELS);
                     $this->INDEX_NAME = $this->SCHEMA_NAME ."_".
                                         $this->TABLE_NAME ."_".
-                                        implode("_",$this->INDEX_COLUMNS) ."_IDX";
+                                        implode("_",$this->INDEX_COLUMNS_LABELS) ."_IDX";
 		}
 		
 		function afterAddRec(){

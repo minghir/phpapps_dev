65a2306d93ba86e9f575c7d01dad7a4e3a448d43
minghir <minghir@gmail.com>
2017-03-07 16:22:07 +0200

    https://github.com/minghir/phpapps_dev.git

--- php_quiz/quiz_test.php
+++ php_quiz/quiz_test.php
@@ -5,12 +5,88 @@ require_once (PHPAPPS_LIBS_DIR . "phpapps_display_abs.php");
 class quiz_test extends phpapps_display_abs{
 
     private $app_id;
+    private $questions = array();
+    private $corect_answers;
     
     function __construct($app_id) {
         parent::__construct();
+        
         $this->tpl = "quiz_test.tpl";        
         $this->app_id = $app_id;
+        $this->corect_answers = 0;
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //print_r($_POST);
+            $this->questions =  $_SESSION["questions_ses"];
+            $answer = $_POST["answer"];
+            //print_r($answer);
+            
+            for($i = 0;$i<count($this->questions);$i++){
+                $this->questions[$i]->user_answer = $answer[$this->questions[$i]->ID];
+                
+                switch($this->questions[$i]->user_answer){
+                    case 1: 
+                         if( $this->questions[$i]->ANSWER_1 == '1'){
+                             $this->corect_answers++;
+                        } 
+                    break;
+                    case 2: 
+                        if( $this->questions[$i]->ANSWER_2 == '1'){
+                             $this->corect_answers++;
+                        } 
+                    break;
+                    case 3: 
+                        if( $this->questions[$i]->ANSWER_3 == '1'){
+                             $this->corect_answers++;
+                        }
+                    break;
+                    case 4:
+                        if( $this->questions[$i]->ANSWER_4 == '1'){
+                             $this->corect_answers++;
+                        }
+                    break;
+              }
+            }
+        }else{
+            $this->setupQueryDisplay();
+        }
+      
         $this->displayTpl();
+    }
+    
+    function setupQueryDisplay(){
+        $sql = new DB_query("SELECT q.ID,
+                                    q.NO,
+                                    q.CATEG_ID,
+                                    (SELECT CONCAT('(<b>',t.VALUE,'</b>) ',c.NAME) 
+                                    FROM php_quiz.categories c LEFT JOIN php_quiz.list_category_type t ON  (c.CATEG_TYPE = t.ID) WHERE c.ID = q.CATEG_ID) AS CTEG_NAME,
+                                    q.QUESTION,
+                                    q.ANSWER_1,
+                                    q.CORRECT_1,
+                                    q.IMAGE_1,
+                                    q.ANSWER_2,
+                                    q.CORRECT_2,
+                                    q.IMAGE_2,
+                                    q.ANSWER_3,
+                                    q.CORRECT_3,
+                                    q.IMAGE_3,
+                                    q.ANSWER_4,
+                                    q.CORRECT_4,
+                                    q.IMAGE_4 
+                            FROM php_quiz.questions q ".$this->where_categ." ORDER BY q.CATEG_ID ASC, q.NO DESC",$this->sql_array);
+        
+        $res_no = $this->globals->con->query($sql);
+        while($res=$this->globals->con->fetch_object()){
+            $this->questions[] = $res;
+        }
+        $_SESSION["questions_ses"] = $this->questions;
+    }    
+     
+    function setupDisplay(){
+        $this->globals->sm->assign(array(
+            "category_id" => $this->category_id,
+            "QUESTIONS" => $this->questions,
+            "CORRECT_ANS" => $this->corect_answers,
+        ));
     }
 }
 
--- php_quiz/tpl/quiz_test.tpl
+++ php_quiz/tpl/quiz_test.tpl
@@ -3,6 +3,28 @@
 {include file="{$PHPAPPS_SMARTY_TPL_DIR}commun_header.tpl" }
 </head>
 <body>
-{$SCRIPT_CONTENT}
+    Raspunsuri corecte:{$CORRECT_ANS} din {count($QUESTIONS)} intrebari.
+    <form name="test" method ="POST">
+{section name=qus loop=$QUESTIONS}
+       <b> {$smarty.section.qus.index + 1} : </b> {stripslashes($QUESTIONS[qus]->QUESTION)}<br>
+       <input type="hidden" name="question_id[]" value="{$QUESTIONS[qus]->ID}">
+       <h1>{$QUESTIONS[qus]->correct_answer}</h1>
+       {if $QUESTIONS[qus]->ANSWER_1 != ""}
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="answer[{$QUESTIONS[qus]->ID}]" value="1">{stripslashes($QUESTIONS[qus]->ANSWER_1)}<br>
+       {/if}
+       {if $QUESTIONS[qus]->ANSWER_2 != ""}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="answer[{$QUESTIONS[qus]->ID}]" value="2">{stripslashes($QUESTIONS[qus]->ANSWER_2)}<br>
+       {/if}
+       {if $QUESTIONS[qus]->ANSWER_3 != ""}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="answer[{$QUESTIONS[qus]->ID}]" value="3">{stripslashes($QUESTIONS[qus]->ANSWER_3)}<br>
+       {/if}
+       {if $QUESTIONS[qus]->ANSWER_4 != ""}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="answer[{$QUESTIONS[qus]->ID}]" value="4">{stripslashes($QUESTIONS[qus]->ANSWER_4)}<br>
+       {/if}
+     <br>
+     <hr>
+ {/section}
+ <input type="submit" name="act" value="Verifica test">
+ <form>
 </body>
 </html>

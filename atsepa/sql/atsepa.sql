444804f081c3b57945dde1e0b46d46a2e9b1a8a3
victor.minghir <victor.minghir@ws-buc-57e5f7.romatsa.ro>
2019-10-25 13:35:44 +0300

    Merge origin/master
    Conflicts:
    	phpapps/libs/DB/DB_cfg.php

--- atsepa/sql/atsepa.sql
+++ atsepa/sql/atsepa.sql
@@ -437,4 +437,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-25  9:25:12
+-- Dump completed on 2019-10-25 13:34:56

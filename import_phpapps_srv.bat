cbe108762712c23d9cb6c24fde64ca481586359b
victor.minghir <victor.minghir@ws-buc-57e5f7.romatsa.ro>
2019-11-11 16:28:34 +0200

    Merge origin/master
    Conflicts:
    	phpapps/libs/DB/DB_cfg.php

--- import_phpapps_srv.bat
+++ import_phpapps_srv.bat
@@ -1,14 +1,23 @@
-type D:\linuxport\work\www\phpapps_dev\phpapps\sql\imp_start.sql D:\linuxport\work\www\phpapps_dev\phpapps\sql\phpapps.sql D:\linuxport\work\www\phpapps_dev\phpapps\sql\imp_end.sql > D:\linuxport\work\www\phpapps_dev\phpapps\sql\import.sql 
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root  -palcatraz phpapps <   D:\work\www\phpapps_dev\phpapps\sql\import.sql
+SET PATH_TO_MYSQL=D:\linuxport\mysql-5.6.26-win32\bin
+SET PATH_TO_IMP_FILES=D:\linuxport\work\www\phpapps_dev
 
-rem D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root   phpapps <  D:\linuxport\work\www\phpapps_dev\phpapps\sql\imp_part.sql
-rem D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root   phpapps <  D:\linuxport\work\www\phpapps_dev\phpapps\sql\phpapps.sql
+rem set list = "phpapps" "eshop_template"
 
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root   eshop <   D:\linuxport\work\www\phpapps_dev\eshop_template\sql\imp_part.sql
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root   eshop <   D:\linuxport\work\www\phpapps_dev\eshop_template\sql\eshop.sql
+SET DATABASE=phpapps
+type %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_start.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\%DATABASE%.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_end.sql > %PATH_TO_IMP_FILES%\%DATABASE%\sql\import.sql 
+%PATH_TO_MYSQL%\mysql.exe --user=root  -palcatraz  <   D:\work\www\phpapps_dev\%DATABASE%\sql\import.sql
 
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root  php_quiz < D:\linuxport\work\www\phpapps_dev\php_quiz\sql\imp_part.sql
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root  php_quiz < D:\linuxport\work\www\phpapps_dev\php_quiz\sql\php_quiz.sql
+SET DATABASE=php_quiz
+type %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_start.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\%DATABASE%.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_end.sql > %PATH_TO_IMP_FILES%\%DATABASE%\sql\import.sql 
+%PATH_TO_MYSQL%\mysql.exe --user=root  -palcatraz  <   D:\work\www\phpapps_dev\%DATABASE%\sql\import.sql
 
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root  atsepa <   D:\linuxport\work\www\phpapps_dev\atsepa\sql\imp_part.sql
-D:\linuxport\mysql-5.6.26-win32\bin\mysql.exe --user=root  atsepa <   D:\linuxport\work\www\phpapps_dev\atsepa\sql\atsepa.sql
+SET DATABASE=atsepa
+type %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_start.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\%DATABASE%.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_end.sql > %PATH_TO_IMP_FILES%\%DATABASE%\sql\import.sql 
+%PATH_TO_MYSQL%\mysql.exe --user=root  -palcatraz  <   D:\work\www\phpapps_dev\%DATABASE%\sql\import.sql
+
+SET DATABASE=eshop_template
+type %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_start.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\%DATABASE%.sql %PATH_TO_IMP_FILES%\%DATABASE%\sql\imp_end.sql > %PATH_TO_IMP_FILES%\%DATABASE%\sql\import.sql 
+%PATH_TO_MYSQL%\mysql.exe --user=root  -palcatraz  <   D:\work\www\phpapps_dev\%DATABASE%\sql\import.sql
+
+pause
+
